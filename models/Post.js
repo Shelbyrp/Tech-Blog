3bf10df15ca7a738357d46edfbff810d6b2f2fdc
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-06-05 13:11:56 +0800

    Started the models.

---
+++ models/Post.js
@@ -0,0 +1,11 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class Post extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+module.exports = Post;
