4bb12919b4197fc157cce962a81b9e5dd9ee57f8
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-06-12 11:42:07 +0800

    Delete comment js update.

--- public/javascript/deletecomment.js
+++ public/javascript/deletecomment.js
@@ -1,14 +1,14 @@
 async function deleteCommentFormHandler(event) {
     event.preventDefault();
-    
+
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
       ];
-    const response = await fetch(`/api/comments/${id}`, {
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+       const response = await fetch(`/api/comments/${id}`, {
         method: 'DELETE',
         body: JSON.stringify({
-          post_id: id,
-          comment_text
+          comment_id: id
         }),
         headers: {
           'Content-Type': 'application/json'
