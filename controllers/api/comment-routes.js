d687325079b26a0f0d57390a2c4cb5599640ec18
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-06-13 13:18:57 +0800

    Included a get and put request for comment.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment, Post, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -9,6 +9,25 @@ router.get('/', async (req, res) => {
   } catch (error) {
     console.log(err);
     res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (request, response) => {
+  try {
+    const CommentData = await Comment.findByPk(
+      request.params.id, {
+      where: {
+        id: request.params.id,
+      },
+    });
+    if (!CommentData) {
+      response.status(404).json({ message: 'No comment found with that id' });
+      return;
+    }
+    response.json(CommentData);
+  } catch (err) {
+    console.log(err);
+    response.status(500).json(err);
   }
 });
 
@@ -24,6 +43,26 @@ router.post('/', withAuth, (req, res) => {
         console.log(err);
         res.status(400).json(err);
       });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    console.log(req.body.comment_text)
+    const commentData = await Comment.update({
+      comment_text: req.body.comment_text,
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with that id!' });
+      return;
+    }
+    res.status(200).json({ message: `Updated comment id #${req.params.id}` });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
   }
 });
 
