f592cad94c72969b47bbc460a41e7b07bc5df887
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-06-14 15:11:05 +0800

    Remove the delete button on the edit comment page.

--- public/javascript/editcomment.js
+++ public/javascript/editcomment.js
@@ -21,28 +21,28 @@ async function editCommentFormHandler(event) {
     }
 }
 
-// async function editSaveCommentFormHandler(event) {
-//     event.preventDefault();
+async function editSaveCommentFormHandler(event) {
+    event.preventDefault();
 
-//     const comment_content = document.querySelector('textarea[name="comment-content"]').value;
-//     let id = this.parentNode.getAttribute('data-id')
+    const comment_content = document.querySelector('textarea[name="comment-content"]').value;
+    let id = this.parentNode.getAttribute('data-id')
 
-//     const response = await fetch(`/api/comments/${id}`, {
-//         method: 'PUT',
-//         body: JSON.stringify({
-//             comment_text
-//         }),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     });
+    const response = await fetch(`/api/comments/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            comment_text
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 
-//     if (response.ok) {
-//         document.location.replace(`/dashboard/edit-comment/${id}`);
-//     } else {
-//         alert(response.statusText);
-//     }
-// }
+    if (response.ok) {
+        document.location.replace(`/dashboard/edit-comment/${id}`);
+    } else {
+        alert(response.statusText);
+    }
+}
 
 
 document.querySelector('.edit-comment-btn').addEventListener('click', editCommentFormHandler);
